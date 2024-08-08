779e844f7af647d40c36d11c4aa57034255794fd
Edgar Gonzalez <edgar.gonzalez.c.p@gmail.com>
2024-08-08 14:04:51 -0700

    Leccion 78

--- 3raParte.py
+++ 3raParte.py
@@ -153,6 +153,7 @@ número menor.
 Si la suma de los 3 números es un valor entre 10 y 15
 (incluidos) va a devolver el número de valorintermedio.
 """
+"""
 def devolver_distintos(a, b, c):
     # Calcular la suma de los tres números
     suma = a + b + c
@@ -175,7 +176,7 @@ print(devolver_distintos(5, 7, 3))   # Salida: 7 (suma=15, número mayor)
 print(devolver_distintos(1, 2, 3))   # Salida: 1 (suma=6, número menor)
 print(devolver_distintos(4, 6, 5))   # Salida: 5 (suma=15, número intermedio)
 
-
+"""
 """
 Escribe una función (puedes ponerle cualquier nombre que
 quieras) que reciba cualquier palabra como parámetro, y que
@@ -184,7 +185,7 @@ alfabético.
 Por ejemplo si al invocar esta función pasamos la palabra
 "entretenido", debería devolver ['d','e','i','n','o','r','t']
 """
-
+"""
 def letras_unicas_ordenadas(palabra):
     # Convertir la palabra a un conjunto para obtener letras únicas
     letras_unicas = set(palabra)
@@ -200,7 +201,7 @@ resultado2 = letras_unicas_ordenadas("programacion")
 print(resultado2)  # Salida: ['a', 'c', 'g', 'i', 'm', 'n', 'o', 'p', 'r']
 
 resultado3 = letras_unicas_ordenadas("python")
-print(resultado3)  # Salida: ['h', 'n', 'o', 'p', 't', 'y']
+print(resultado3)  # Salida: ['h', 'n', 'o', 'p', 't', 'y']"""
 
 """
 Escribe una función que requiera una cantidad indefinida de
@@ -211,7 +212,7 @@ Por ejemplo:
 (5,6,1,0,0,9,3,5) >>> True
 (6,0,5,1,0,3,0,1) >>> False
 """
-
+"""
 def cero_repetido_consecutivo(*args):
     # Iterar a través de los argumentos
     for i in range(len(args) - 1):
@@ -226,7 +227,7 @@ print(cero_repetido_consecutivo(6, 0, 5, 1, 0, 3, 0, 1))  # Salida: False
 print(cero_repetido_consecutivo(1, 2, 3, 4, 5))            # Salida: False
 print(cero_repetido_consecutivo(0, 0, 0, 0))               # Salida: True
 
-
+"""
 
 """
 Escribe una función llamada contar_primos() que requiera un
@@ -238,5 +239,54 @@ primos que encontró.
 Aclaración, por convención el 0 y el 1 no se consideran primos
 """
 
+# DIA 6 - PROGRAMA UN RECETARIO
+# Abrir y manipular archivos - Leccion 77 Solo se modifica en esta leccion las variables que clonan al archivo
+# open, read, write, close - Abrir archivos y alojarlos en varibales para aplicar metodos.
+"""
+mi_archivo = open("prueba.txt") #Al estar alojado en la misma carpeta, no es necesario decirle que ruta
+print(mi_archivo) #
+print(mi_archivo.read()) #
+
+una_linea = mi_archivo.readline()
+print(una_linea.upper()) #Se pueden usar metodos de los string
+una_linea = mi_archivo.readline() #Lee a partir de donde se quedó #De esta forma no se procesa el salto de linea 
+print(una_linea.rstrip)
+una_linea = mi_archivo.readline()
+print(una_linea) 
+
+#Iterar:
+
+for l in mi_archivo:
+    print("Aqui dice: "+l)
+
+#Metodo readlines para convertirlos en listas
+
+todas = mi_archivo.readlines() #Devuelve una lista con cada linea #Es mejor usar readlines para archivos pequeños.
+todas = todas.pop() 
+'''El método pop() elimina y retorna un elemento de una lista.
+#Hay un parámetro opcional, el índice a ser eliminado de la lista, 
+# si no se especifica ningún índice, a.pop() elimina y retorna el último elemento de la lista.'''
+print(todas)
+mi_archivo.close() #Es recomendable cerrarlo para no ocupar memoria
+"""
+# Crear y escribir archivos Leccion 78
+# El metodo open open("archivo.txt","x") x {modo apertura} = r {solo lectura}, = w {solo escritura, se crea si no existe, y si existe se re
+# reseta, osea borra(sobreescribe) todas las lineas y posiciona cursor al inicio}, a = {solo escritura, se crea si no existe, y si existe posiciona el cursor 
+# hasta el final del archivo para escribir apartir de ahi, mantiene el contenido}
+
+archivo = open("prueba1.txt","w") #Solo lectura, al no encontrarlo va a crear uno
+archivo.write("Soy el nuevo texto\n") #Metodo para escribir, no permitira si se abrio modo lectura "r"
+archivo.write('''Soy la segunda linea
+yo la tercera con salto de linea usando diferente metodo al caracter de escape \\n''')
+archivo.write("\n") #Salto de linea simple
+archivo.writelines(["Una","lista","escrita","con","metodo","writelines\n"]) #Escribe los elementos juntos de corrido sin salto
+lista = ["Una","lista","escrita","con","metodo","writelines"]
+for p in lista: #Otra forma de escribir una lista con un for y metodo write
+    archivo.write(p + "\n") 
+archivo.close()
+
+archivo = open("prueba1.txt","a")
+archivo.write("Bienvenido")
+archivo.close()
 
 
