7d6649d0f694020a230d4c22de3c7f4b73acb5dd
Edgar Gonzalez <edgar.gonzalez.c.p@gmail.com>
2024-08-15 01:04:45 -0700

    Leccion96

---
+++ Leccion95_Clases.py
@@ -0,0 +1,14 @@
+
+class Carro: #Clase vacia
+    pass
+
+class Pajaro:
+    pass
+mi_pajaro = Pajaro()
+otro_pajaro = Pajaro()
+
+print(type(mi_pajaro))
+print(otro_pajaro)
+
+
+
---
+++ Leccion96_Atributos.py
@@ -0,0 +1,21 @@
+#Dos clases de atributos en la POO en Python
+#Atributos de clase
+#Atributos de la instancia
+
+class Pajaro:
+    alas =True #Atributo de clase
+    def __init__(self,color): #Metodo constructor que asigna atributo, afuerzas en la instancia pedira un atributo a asignar
+        self.color=color #Al atributo color le asignamos el valor de la variable color
+
+mi_pajaro=Pajaro("negro")
+
+class Auto:
+    def __init__(self,color,marca):
+        self.color = color
+        self.marca = marca
+
+mi_auto = Auto("negro","Toyota")
+print(f"Mi auto es un {mi_auto.marca} de color {mi_auto.color}")
+
+print(Pajaro.alas)
+print(mi_pajaro.alas)
